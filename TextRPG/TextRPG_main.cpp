146c18957194cf667347dd28d5913643cb741206
JasonChoi <ccc11203@gmail.com>
2018-11-27 21:58:59 +0900

    TextRPG, Store, Inventory

--- TextRPG/TextRPG_main.cpp
+++ TextRPG/TextRPG_main.cpp
@@ -39,10 +39,46 @@ enum BATTLE
 	BATTLE_BACK
 };
 
+
+enum ITEM_TYPE
+{
+	IT_NONE,
+	IT_WEAPON,
+	IT_ARMOR,
+	IT_BACK
+};
+
+
+enum STORE_MENU
+{
+	SM_NONE,
+	SM_WEAPON,
+	SM_ARMOR,
+	SM_BACK
+};
+
+
 #define NAME_SIZE 32
+#define ITEM_DESC_LENGTH 512
+#define INVENTORY_MAX 20
+#define STORE_WEAPONE_MAX 3
+#define STORE_ARMOR_MAX 3
+struct _tagItem
+{
+	char	strName[NAME_SIZE];
+	char	strTypeName[NAME_SIZE];
+	ITEM_TYPE eType;
+	int		iMin;
+	int		iMax;
+	int		iPrice;
+	int		iSell;
+	char	strDesc[ITEM_DESC_LENGTH];
+};
 
 struct _tagInventory
 {
+	_tagItem tItem[INVENTORY_MAX];
+	int		iItemCount;
 	int		iGold;
 
 };
@@ -206,6 +242,68 @@ int main()
 	tMonsterArr[2].iExp = 30000;
 	tMonsterArr[2].iGoldMin = 20000;
 	tMonsterArr[2].iGoldMax = 50000;
+	
+	//construct store item list
+	_tagItem tStoreWeapon[STORE_WEAPONE_MAX] = {};
+	_tagItem tStoreArmor[STORE_ARMOR_MAX] = {};
+
+	//set item info
+	strcpy_s(tStoreWeapon[0].strName, "Doran Blade");
+	strcpy_s(tStoreWeapon[0].strTypeName, "Blade");
+	strcpy_s(tStoreWeapon[0].strDesc, "This doran blade is pretty normal but quite useful.");
+	tStoreWeapon[0].eType = IT_WEAPON;
+	tStoreWeapon[0].iMin = 2;
+	tStoreWeapon[0].iMax = 5;
+	tStoreWeapon[0].iPrice = 5000;
+	tStoreWeapon[0].iSell = 3000;
+
+	strcpy_s(tStoreWeapon[1].strName, "Sin Orb");
+	strcpy_s(tStoreWeapon[1].strTypeName, "Orb");
+	strcpy_s(tStoreWeapon[1].strDesc, "Shining and beautiful Orb for Wizard but even Knight can use it.");
+	tStoreWeapon[1].eType = IT_WEAPON;
+	tStoreWeapon[1].iMin = 3;
+	tStoreWeapon[1].iMax = 50;
+	tStoreWeapon[1].iPrice = 50000;
+	tStoreWeapon[1].iSell = 30000;
+
+
+	strcpy_s(tStoreWeapon[2].strName, "Chaos Axe");
+	strcpy_s(tStoreWeapon[2].strTypeName, "Axe");
+	strcpy_s(tStoreWeapon[2].strDesc, "Sharp and heavy Axe.");
+	tStoreWeapon[2].eType = IT_WEAPON;
+	tStoreWeapon[2].iMin = 10;
+	tStoreWeapon[2].iMax = 20;
+	tStoreWeapon[2].iPrice = 7000;
+	tStoreWeapon[2].iSell = 5000;
+
+
+
+	strcpy_s(tStoreArmor[0].strName, "Chain Mail");
+	strcpy_s(tStoreArmor[0].strTypeName, "Armor");
+	strcpy_s(tStoreArmor[0].strDesc, "light but firm armor");
+	tStoreArmor[0].eType = IT_ARMOR;
+	tStoreArmor[0].iMin = 5;
+	tStoreArmor[0].iMax = 8;
+	tStoreArmor[0].iPrice = 2000;
+	tStoreArmor[0].iSell = 1000;
+
+	strcpy_s(tStoreArmor[1].strName, "Field Plate");
+	strcpy_s(tStoreArmor[1].strTypeName, "Armor");
+	strcpy_s(tStoreArmor[1].strDesc, "Nice quality armor");
+	tStoreArmor[1].eType = IT_ARMOR;
+	tStoreArmor[1].iMin = 10;
+	tStoreArmor[1].iMax = 20;
+	tStoreArmor[1].iPrice = 3000;
+	tStoreArmor[1].iSell = 2000;
+
+	strcpy_s(tStoreArmor[2].strName, "Gothic Plate");
+	strcpy_s(tStoreArmor[2].strTypeName, "Armor");
+	strcpy_s(tStoreArmor[2].strDesc, "Heavy, firm, Good quality armor");
+	tStoreArmor[2].eType = IT_ARMOR;
+	tStoreArmor[2].iMin = 40;
+	tStoreArmor[2].iMax = 50;
+	tStoreArmor[2].iPrice = 50000;
+	tStoreArmor[2].iSell = 10000;
 
 
 	while (1)
@@ -377,8 +475,195 @@ int main()
 			}
 			break;
 		case MM_STORE:
+			while (1)
+			{
+				system("cls");
+				cout << "================= store =================" << endl;
+				cout << "1. weapon store" << endl;
+				cout << "2. armor store" << endl;
+				cout << "3. back" << endl;
+				cout << "select store : ";
+				cin >> iMenu;
+
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+
+				else if (iMenu == SM_BACK)
+					break;
+
+				switch (iMenu)
+				{
+				case SM_WEAPON:
+					while (1)
+					{
+						system("cls");
+
+						//show selling items
+						cout << "================= weapon store =================" << endl;
+						for (int i = 0; i < STORE_WEAPONE_MAX; ++i)
+						{
+							cout << i + 1 << "." << tStoreWeapon[i].strName << "\t type : " << tStoreWeapon[i].strTypeName << endl;
+							cout << "plus damage : " << tStoreWeapon[i].iMin << "-" << tStoreWeapon[i].iMax << endl;
+							cout << "original price : " << tStoreWeapon[i].iPrice << "G" << "\tPurchase : " << tStoreWeapon[i].iSell << "G"<< endl;
+							cout << tStoreWeapon[i].strDesc << endl;
+							cout << endl;
+						}
+						cout << STORE_WEAPONE_MAX + 1 << ".go Back to store menu" << endl;
+						cout << endl;
+						cout << "Inventory Gold : " << tPlayer.tInventory.iGold << "G" << endl;
+						cout << "Inventory space : " << tPlayer.tInventory.iItemCount << "/" << INVENTORY_MAX << endl;
+						cout << "Select item to purchase : ";
+						cin >> iMenu;
+						if (cin.fail())
+						{
+							cin.clear();
+							cin.ignore(1024, '\n');
+							continue;
+						}
+
+						else if (iMenu == STORE_WEAPONE_MAX + 1)
+							break;
+
+						else if (iMenu < 1 || iMenu > STORE_WEAPONE_MAX + 1)
+						{
+							cout << "Wrong input." << endl;
+							system("pause");
+							continue;
+						}
+
+						
+						int iWeaponIndex = iMenu - 1;
+
+
+						//Check inventory is full
+						if (tPlayer.tInventory.iItemCount == INVENTORY_MAX)
+						{
+							cout << "Inventory is full." << endl;
+							system("pause");
+							continue;
+						}
+
+						else if (tPlayer.tInventory.iGold < tStoreWeapon[iWeaponIndex].iPrice)
+						{
+							cout << "Not enough Gold." << endl;
+							system("pause");
+							continue;
+						}
+
+						//when purchase item, item go to inventory[0], count become 1 and 
+						//next purchase item go to inventory[1]
+						tPlayer.tInventory.tItem[tPlayer.tInventory.iItemCount] =
+							tStoreWeapon[iWeaponIndex];
+						++tPlayer.tInventory.iItemCount;
+
+						//decrease Gold
+						tPlayer.tInventory.iGold -= tStoreWeapon[iWeaponIndex].iPrice;
+
+						cout << tStoreWeapon[iWeaponIndex].strName << " purchased." << endl;
+						system("pause");
+					}
+					break;
+				case SM_ARMOR:
+					while (1)
+					{
+						system("cls");
+
+						//show selling items
+						cout << "================= armor store =================" << endl;
+						for (int i = 0; i < STORE_ARMOR_MAX; ++i)
+						{
+							cout << i + 1 << "." << tStoreArmor[i].strName << "\t type : " << tStoreArmor[i].strTypeName << endl;
+							cout << "plus damage : " << tStoreArmor[i].iMin << "-" << tStoreArmor[i].iMax << endl;
+							cout << "original price : " << tStoreArmor[i].iPrice << "G" << "\tPurchase : " << tStoreArmor[i].iSell << "G" << endl;
+							cout << tStoreArmor[i].strDesc << endl;
+							cout << endl;
+						}
+						cout << STORE_ARMOR_MAX + 1 << ".go Back to store menu" << endl;
+						cout << endl;
+						cout << "Inventory Gold : " << tPlayer.tInventory.iGold << "G" << endl;
+						cout << "Inventory space : " << tPlayer.tInventory.iItemCount << "/" << INVENTORY_MAX << endl;
+						cout << "Select item to purchase : ";
+						cin >> iMenu;
+						if (cin.fail())
+						{
+							cin.clear();
+							cin.ignore(1024, '\n');
+							continue;
+						}
+
+						else if (iMenu == STORE_ARMOR_MAX + 1)
+							break;
+
+						else if (iMenu < 1 || iMenu > STORE_ARMOR_MAX + 1)
+						{
+							cout << "Wrong input." << endl;
+							system("pause");
+							continue;
+						}
+
+
+						int iArmorIndex = iMenu - 1;
+
+
+						//Check inventory is full
+						if (tPlayer.tInventory.iItemCount == INVENTORY_MAX)
+						{
+							cout << "Inventory is full." << endl;
+							system("pause");
+							continue;
+						}
+
+						else if (tPlayer.tInventory.iGold < tStoreArmor[iArmorIndex].iPrice)
+						{
+							cout << "Not enough Gold." << endl;
+							system("pause");
+							continue;
+						}
+
+						//when purchase item, item go to inventory[0], count become 1 and 
+						//next purchase item go to inventory[1]
+						tPlayer.tInventory.tItem[tPlayer.tInventory.iItemCount] =
+							tStoreArmor[iArmorIndex];
+						++tPlayer.tInventory.iItemCount;
+
+						//decrease Gold
+						tPlayer.tInventory.iGold -= tStoreArmor[iArmorIndex].iPrice;
+
+						cout << tStoreArmor[iArmorIndex].strName << " purchased." << endl;
+						system("pause");
+					}
+					break;
+				}
+			}
 			break;
 		case MM_INVENTORY:
+			system("cls");
+			cout << "************************ inventory ************************" << endl;
+			cout << "======================= Player =======================" << endl;
+			cout << "name : " << tPlayer.strName << "\tJob : " << tPlayer.strJobName << endl;
+			cout << "level : " << tPlayer.iLevel << "\tExp : " << tPlayer.iExp << endl;
+			cout << "Attack : " << tPlayer.iAttackMin << " - " << tPlayer.iAttackMax << endl;
+			cout << "Defense : " << tPlayer.iArmorMin << " - " << tPlayer.iArmorMax << endl;
+			cout << "HP : " << tPlayer.iHP << "/" << tPlayer.iHPMax << "\tMP : " << tPlayer.iMP << "/" << tPlayer.iMPMax << endl;
+			cout << "Gold : " << tPlayer.tInventory.iGold << "G" << endl;
+			if (tPlayer.tInventory.iItemCount == 0)
+			{
+				cout << "You have no item in inventory" << endl;
+				system("pause");
+				break;
+			}
+			for (int i = 0; i < tPlayer.tInventory.iItemCount; ++i)
+			{
+				cout << i + 1 << "." << tPlayer.tInventory.tItem[i].strName << "\t" << tPlayer.tInventory.tItem[i].strTypeName << endl;
+				cout << "plus damage : " << tPlayer.tInventory.tItem[i].iMin << "-" << tPlayer.tInventory.tItem[i].iMax << endl;
+				cout << tPlayer.tInventory.tItem[i].strDesc << endl;
+				cout << endl;
+			}
+			system("pause");
 			break;
 		default:
 			cout << "Wrong input, please try again" << endl;
